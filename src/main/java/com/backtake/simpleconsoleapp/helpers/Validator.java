ac23c34bda0d14f5fd5d69f6c1229985737ac7b7
backtake <stasiusoltysik@gmail.com>
2018-07-09 10:35:41 +0200

    Add login validation

--- src/main/java/com/backtake/simpleconsoleapp/helpers/Validator.java
+++ src/main/java/com/backtake/simpleconsoleapp/helpers/Validator.java
@@ -1,4 +1,35 @@
 package com.backtake.simpleconsoleapp.helpers;
 
+import com.backtake.simpleconsoleapp.user.User;
+import com.backtake.simpleconsoleapp.user.UserRepository;
+import com.backtake.simpleconsoleapp.view.View;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class Validator {
+
+    private UserRepository repository;
+    private View view;
+
+    @Autowired
+    public Validator(UserRepository repository, View view) {
+        this.repository = repository;
+        this.view = view;
+    }
+
+    public boolean isLoginValid(String login) {
+        List<User> users = (List<User>) repository.findAll();
+        if (login.length()<3) {
+            view.displayCreateInfoNotLongEnough();
+            return false;
+        }
+        for(User u: users) {
+            if(login.equals(u.getLogin())){
+                view.displayCreateLoginInfoIfAlreadyExists();
+                return false;
+            }
+        }
+        return true;
+    }
 }
