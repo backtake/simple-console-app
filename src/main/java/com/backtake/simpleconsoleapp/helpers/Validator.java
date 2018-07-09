1d5c8c92464a00d19af06a38ed27c27b1b622db9
backtake <stasiusoltysik@gmail.com>
2018-07-09 11:32:12 +0200

    Add @Service

--- src/main/java/com/backtake/simpleconsoleapp/helpers/Validator.java
+++ src/main/java/com/backtake/simpleconsoleapp/helpers/Validator.java
@@ -4,9 +4,11 @@ import com.backtake.simpleconsoleapp.user.User;
 import com.backtake.simpleconsoleapp.user.UserRepository;
 import com.backtake.simpleconsoleapp.view.View;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class Validator {
 
     private UserRepository repository;
