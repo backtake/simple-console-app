285f4e00f85573ba5686f618f798f8810d163778
backtake <stasiusoltysik@gmail.com>
2018-07-09 10:41:15 +0200

    Create AppController with imports and constructor

---
+++ src/main/java/com/backtake/simpleconsoleapp/controllers/AppController.java
@@ -0,0 +1,29 @@
+package com.backtake.simpleconsoleapp.controllers;
+
+import com.backtake.simpleconsoleapp.helpers.Validator;
+import com.backtake.simpleconsoleapp.helpers.passwordutils.EncryptPassword;
+import com.backtake.simpleconsoleapp.helpers.passwordutils.VerifyUserPassword;
+import com.backtake.simpleconsoleapp.user.UserRepository;
+import com.backtake.simpleconsoleapp.view.View;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AppController {
+
+    private View view;
+    private UserRepository repository;
+    private Validator validator;
+    private EncryptPassword encryptPassword;
+    private VerifyUserPassword verifyUserPassword;
+
+    @Autowired
+    public AppController(View view, UserRepository repository, Validator validator, EncryptPassword encryptPassword,
+                         VerifyUserPassword verifyUserPassword) {
+        this.view = view;
+        this.repository = repository;
+        this.validator = validator;
+        this.encryptPassword = encryptPassword;
+        this.verifyUserPassword = verifyUserPassword;
+    }
+
+
+}
